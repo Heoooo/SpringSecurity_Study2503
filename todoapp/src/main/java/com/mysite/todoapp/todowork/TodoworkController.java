3e86d8a82e47b5957e54cbf09a3487a99a9ef33d
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-03-27 17:14:35 +0900

    0327:App1 Todotitle 상세페이지 실습3

--- todoapp/src/main/java/com/mysite/todoapp/todowork/TodoworkController.java
+++ todoapp/src/main/java/com/mysite/todoapp/todowork/TodoworkController.java
@@ -1,5 +1,31 @@
 package com.mysite.todoapp.todowork;
 
-public class TodoworkController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysite.todoapp.todotitle.Todotitle;
+import com.mysite.todoapp.todotitle.TodotitleService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/todowork")
+public class TodoworkController {
+	
+	private final TodotitleService todotitleService;
+	private final TodoworkService todoworkService;
+	
+	@PostMapping("/create/{id}")
+	public String createTodowork(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+		
+		Todotitle todotitle = todotitleService.getTodotitle(id);
+		todoworkService.create(todotitle, content);
+		
+		return String.format("redirect:/todotitle/detail/%s", id);
+	}
 }
