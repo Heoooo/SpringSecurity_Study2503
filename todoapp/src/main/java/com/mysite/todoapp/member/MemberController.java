343ec2d38131d761bb3e408412b04a7ed8ea1b4e
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-03-29 20:07:28 +0900

    0329:App2 Member 실습2 - Member Controller

--- todoapp/src/main/java/com/mysite/todoapp/member/MemberController.java
+++ todoapp/src/main/java/com/mysite/todoapp/member/MemberController.java
@@ -1,5 +1,27 @@
 package com.mysite.todoapp.member;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	//로그인 요청->/member/login
+	@GetMapping("/member/login")
+	public String login() {
+		
+		return "login_form";
+	}
+	
+	//회원가입 요청->/member/signup
+	@GetMapping("/member/signup")
+	public String signup() {
+		
+		return "signup_form";
+	}
 }
